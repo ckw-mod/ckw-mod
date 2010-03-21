4188e238d0d5d2c0e3a638392d21ece04119b186
Deflis <deflis@gmail.com>
2010-03-22 01:03:29 +0900

    Release of mod+git-deflis-2.

--- version.h
+++ version.h
@@ -1,8 +1,8 @@
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 8
 #define VERSION_BUGFIX 10
-#define VERSION_STRING "0.8.10-mod+git"
+#define VERSION_STRING "0.8.10-mod+git-deflis-2"
 #define VERSION_NUMSTRING "008101"
-#define BUILDDATE_STRING "2009/1/25 15:10 UTC+9"
-#define COPYRIGHT_STRING "ckw original - Copyright (C) 2005  Kazuo Ishii <k-ishii@wb4.so-net.ne.jp>\r\n"
-#define ABOUT_DLG_STRING "ckw version 0.8.10-mod+git-deflis"
+#define BUILDDATE_STRING "2010/03/22  0:56:48 UTC+9"
+#define COPYRIGHT_STRING "Copyright (C) 2005-2010  Kazuo Ishii and hideden and shuujin and s-yata and Shimizukawa and Deflis"
+#define ABOUT_DLG_STRING "ckw version 0.8.10-mod+git-deflis-2 , build 2010/03/22  0:56:48 UTC+9\r\nCopyright (C) 2005-2010  Kazuo Ishii and ckw-moders"
