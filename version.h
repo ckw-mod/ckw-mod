a7a9e1f9f615cd379e234cb403bfa283e8e288cd
NANRI <southly@gmail.com>
2012-06-02 23:16:17 +0900

    fix typo

--- version.h
+++ version.h
@@ -4,5 +4,5 @@
 #define VERSION_STRING "0.9.0-d2"
 #define VERSION_NUMSTRING "009002"
 #define BUILDDATE_STRING "2010/07/18 14:32:14 UTC+9"
-#define COPYRIGHT_STRING "Copyright (C) 2005-2010  Kazuo Ishii and ckw-mod creaters and more..."
+#define COPYRIGHT_STRING "Copyright (C) 2005-2010  Kazuo Ishii and ckw-mod creators and more..."
 #define ABOUT_DLG_STRING "ckw-mod version 0.9.0-d2 , build 2010/07/18 14:32:14 UTC+9\r\nCopyright (C) 2005-2010  Kazuo Ishii and ckw-moders"
