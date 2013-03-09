149c9648588f2f1a323b9115a4958b80d3f29087
NANRI <southly@gmail.com>
2013-03-10 02:06:43 +0900

    version.h を更新 (#11)

--- version.h
+++ version.h
@@ -1,8 +1,8 @@
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 9
-#define VERSION_BUGFIX 0
-#define VERSION_STRING "0.9.0-d2"
-#define VERSION_NUMSTRING "009002"
-#define BUILDDATE_STRING "2010/07/18 14:32:14 UTC+9"
+#define VERSION_BUGFIX 1
+#define VERSION_STRING "0.9.1"
+#define VERSION_NUMSTRING "00901"
+#define BUILDDATE_STRING "2013/03/10  1:47:55 UTC+9"
 #define COPYRIGHT_STRING "Copyright (C) 2005-2010  Kazuo Ishii and ckw-mod creators and more..."
-#define ABOUT_DLG_STRING "ckw-mod version 0.9.0-d2 , build 2010/07/18 14:32:14 UTC+9\r\nCopyright (C) 2005-2010  Kazuo Ishii and ckw-moders"
+#define ABOUT_DLG_STRING "ckw-mod version 0.9.1 , build 2013/03/10  1:47:55 UTC+9\r\nCopyright (C) 2005-2010  Kazuo Ishii and ckw-moders"
